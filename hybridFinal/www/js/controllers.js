3d1e79f0ea8988b6709e017f6c8e471784fd644d
Robin Verheijen <info@verrodesigns.nl>
2015-04-01 13:40:18 +0200

    Working map

--- hybridFinal/www/js/controllers.js
+++ hybridFinal/www/js/controllers.js
@@ -3,32 +3,88 @@ angular.module('starter.controllers', [])
         .controller('DashCtrl', function ($scope) {
         })
 
-        .controller('MapCtrl', function ($scope, $ionicLoading) {
-            
-//            google.maps.event.addDomListener(window, 'load', function () {
-                
-                var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+        .controller('MapCtrl', function ($scope, $ionicLoading, $compile) {
 
+//            google.maps.event.addDomListener(window, 'load', function () {
+
+//            var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+//
+//            var mapOptions = {
+//                center: myLatlng,
+//                zoom: 16,
+//                mapTypeId: google.maps.MapTypeId.ROADMAP
+//            };
+//
+//            var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+//
+//            navigator.geolocation.getCurrentPosition(function (pos) {
+//
+//                map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+//                var myLocation = new google.maps.Marker({
+//                    position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+//                    map: map,
+//                    title: "My Location"
+//                });
+//            });
+
+//            $scope.map = map;
+//            });
+
+            function initialize() {
+                
                 var mapOptions = {
-                    center: myLatlng,
                     zoom: 16,
                     mapTypeId: google.maps.MapTypeId.ROADMAP
                 };
+                
+                var map = new google.maps.Map(document.getElementById("map"),
+                        mapOptions);
 
-                var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+                //Marker + infowindow + angularjs compiled ng-click
+                var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+                var compiled = $compile(contentString)($scope);
 
-                navigator.geolocation.getCurrentPosition(function (pos) {
-                    
-                    map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-                    var myLocation = new google.maps.Marker({
-                        position: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
-                        map: map,
-                        title: "My Location"
-                    });
+                var infowindow = new google.maps.InfoWindow({
+                    content: compiled[0]
                 });
 
                 $scope.map = map;
-//            });
+                
+                $ionicLoading.show({
+                    content: 'Getting current location...',
+                    showBackdrop: false
+                });
+                setTimeout(function() {
+                    navigator.geolocation.getCurrentPosition(function (pos) {
+                        $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                        $ionicLoading.hide();
+
+                        $scope.currentPos = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+
+                        var marker = new google.maps.Marker({
+                            position: $scope.currentPos,
+                            map: map,
+                            title: 'Uluru (Ayers Rock)'
+                        });
+
+                        google.maps.event.addListener(marker, 'click', function () {
+                            infowindow.open(map, marker);
+                        });
+
+                    }, function (error) {
+                        alert('Unable to get location: ' + error.message);
+                    });
+                }, 500);
+                
+            }
+            
+            ionic.Platform.ready(initialize);
+//            initialize();
+//            google.maps.event.addDomListener(window, 'load', initialize);
+
+            $scope.clickTest = function () {
+                alert('Example of infowindow with ng-click');
+            };
         })
         .controller('ChatsCtrl', function ($scope, Chats) {
             $scope.chats = Chats.all();
